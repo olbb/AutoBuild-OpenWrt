cae973fcf9880b6988d0b30a62e1a64565258849
JuanLv <5554102+olbb@users.noreply.github.com>
2023-07-10 14:47:20 +0800

    customiz.sh

--- customize.sh
+++ customize.sh
@@ -13,3 +13,6 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
+
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
